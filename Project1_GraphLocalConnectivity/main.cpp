f1bfc295219879c294bdb6950ccee07b3778fd9e
YI LING <Alex@r01ylz29.device.mst.edu>
2014-01-27 11:51:16 -0600

    Initial Commit

---
+++ Project1_GraphLocalConnectivity/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project1_GraphLocalConnectivity
+//
+//  Created by YI LING on 1/27/14.
+//  Copyright (c) 2014 YI LING. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
